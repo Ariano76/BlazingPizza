b89ba58ca0542169818c7e3432ba061b911857e4
Percy Herrera <percyher@hotmail.com>
2020-12-02 22:29:38 -0500

    02/12/2020 22:29

--- BlazingPizza/Server/Models/PizzaStoreContext.cs
+++ BlazingPizza/Server/Models/PizzaStoreContext.cs
@@ -10,10 +10,26 @@ namespace BlazingPizza.Server.Models
     public class PizzaStoreContext : DbContext
     {
         public DbSet<PizzaSpecial> Specials { get; set; }
+        public DbSet<Topping> Toppings { get; set; }
+        public DbSet<Pizza> Pizzas { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"Server=.;Database=PizzaStore;Trusted_Connection=True");
             //optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;database=PizzaStore;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Definir la llave primaria de la entidad PizzaTopping
+            modelBuilder.Entity<PizzaTopping>()
+                .HasKey(pst => new { pst.PizzaId, pst.ToppingId });
+            // Una Pizza puede tener muchos Toppings.
+            modelBuilder.Entity<PizzaTopping>()
+                .HasOne<Pizza>().WithMany(ps => ps.Toppings);
+            // Un Topping puede estar en muchas Pizzas.
+            modelBuilder.Entity<PizzaTopping>()
+                .HasOne(pst => pst.Topping).WithMany();                
         }
     }
 }
